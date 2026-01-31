1e9ed340ef3f1f8226a94f30ff0554cd342c5905
Roderick Liao <liaoroderickpersonal@gmail.com>
2026-01-31 13:31:28 -0500

    Moved Files
    Moved files

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <TCS3200.h>
+#include "constants.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
+TCS3200 tcs3200(clr::S0, clr::S1, clr::S2, clr::S3, clr::S_Out);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+String color_indices[] = {"Red", "Green", "Blue"};
+
+RGBColor color_values[] = {
+  {255, 0, 0},  // Red
+  {0, 255, 0},  // Green
+  {0, 0, 255},  // Blue
+};
+
+void setup(){
+
+    tcs3200.begin();
+    tcs3200.frequency_scaling(TCS3200_OFREQ_20P);
+
+    Serial.begin(115200);
+
+    delay(1000);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    RGBColor rgb_color = tcs3200.read_rgb_color();
+
+    Serial.println("Red: " + String(rgb_color.red));
+    Serial.println("Green: " + String(rgb_color.green));
+    Serial.println("Blue: " + String(rgb_color.blue));
+
+    Serial.println("-----------------------------------");
+
+    delay(1000);
+
 }
